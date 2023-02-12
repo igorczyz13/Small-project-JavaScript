a2737719ac22d02229bcf96176ba198fddaea184
Igor Czyż <igorczyz13@gmail.com>
2023-02-12 17:28:25 +0100

    Task 2.3

--- js/function5.js
+++ js/function5.js
@@ -19,7 +19,7 @@ const oddMsg = (params) => {
     console.log(`liczba ${score} jest nieparzysta.`);
 }
 
-add(10,11)
+add(10, 11)
 
 // ---------------------------------------- Task 2.2
 //stworz zeminne celsius oraz temp, stworz funkcje fahrenheit, ktora przyjmie jeden parametr, argument ma zostac przypisany do zmiennej celsius(wewnatrz funkcji), funkcja ma przekonwertowac C na F, wyniki przypisz do zmiennej temp. wzor: T(F) = T(C) x 1,8 + 32. wywolaj funkcje, podaj do niej jakies argumenty(typ number czyli nasza temperature). wynik wypisz w konsoli za pomoca template stringa, np X C = Y F
@@ -36,3 +36,7 @@ const fahrenheit = (c) => {
 }
 
 fahrenheit(20)
+
+// ---------------------------------------- Task 2.3
+// Stworz zmienne num oraz numbers. do tej pierwszej przypisz wartosc 10, a do drugiej pusta tablice, stworz petle for, ktora bedzie wykonywala sie tak dlugo, az i bedzie mniejsza od num, petla ma dodawac i do tablicy numbers. stworz funkcje, ktora przyjmie jeden parametr, w funkcji ma byc if, ktory sprawdzi, czy liczba podana w argumnecie jest podzielna przez 3 oraz jest rozna od zera. jesli tak - wypisz w konsoli x jest podzielne przez 3, jesli nie - wypisz w konsoli x nie jest podzielne przez 3 lub x = 0. za pomoca forEach odwolaj sie do tablicy numbers
+
