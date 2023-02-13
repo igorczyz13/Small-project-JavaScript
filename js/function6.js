9fd4319f8766fb09214e70c73c269a22d06f4282
Igor Czyż <igorczyz13@gmail.com>
2023-02-13 17:11:33 +0100

    Task 2.4

--- js/function6.js
+++ js/function6.js
@@ -1,16 +1,54 @@
-// getElementById
+// // getElementById
 
-const test = document.getElementById('item')
+// const test = document.getElementById('item')
 
-console.log(test);
+// console.log(test);
 
-// getElementsByTagName
+// // getElementsByTagName
 
-const test2 = document.getElementsByTagName('ul')
-console.log(test2);
+// const test2 = document.getElementsByTagName('ul')
+// console.log(test2);
 
 
-// getElementsByClassName
+// // getElementsByClassName
 
-const test3 = document.getElementsByClassName('test')
-console.log(test3);
+// const test3 = document.getElementsByClassName('test')
+// console.log(test3);
+
+
+// querySelector (ES6)
+// const test = document.querySelector('li')
+// console.log(test);
+
+// const ulList = document.querySelector('ul')
+// console.log(ulList);
+
+// const liItem = ulList.querySelector('li')
+// console.log(liItem);
+
+// // querySelectorAll (ES6)
+
+// const liItems = document.querySelectorAll('li')
+// console.log(liItems);
+
+
+// // zywe kolekcje, czyli qSA vs getElemntsByTag / ClassName
+
+// const newLi = document.createElement('li')
+// ulList.appendChild(newLi).textContent = 'hu'
+
+// console.log(liItems.length);
+
+// --------------------------------------------------- Task 2.4
+
+const heading = document.querySelector('h1')
+console.log(heading);
+
+const paragraf = document.querySelectorAll('p')
+console.log(paragraf);
+
+const testDiv= document.querySelector('.test')
+console.log(testDiv);
+
+const jeff = testDiv.querySelector('#test')
+console.log(jeff);
