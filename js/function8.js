02c3684b680db420eb59ab8348a713d7e5d9b311
Igor Czyż <igorczyz13@gmail.com>
2023-02-13 21:48:53 +0100

    Task 2.6

--- js/function8.js
+++ js/function8.js
@@ -1,3 +1,4 @@
+//----------------------------------------------------Task 2.5
 // stworz w js ul liste i dodaj ja na strone
 // lista ma zawierac w sobie 10 tagow li, w kazdym li ma byc wypisana cyfra 1,2,3..
 // pobierz ostatni element listy, ale nie dodawaj do niego zadnej klasy / ID!
@@ -20,3 +21,17 @@ lastLi.textContent = 'jestem ostatnim elementem'
 lastLi.style.backgroundColor = 'blue'
 lastLi.style.padding = '20px 40px'
 lastLi.style.fontSize = '48px'
+
+//----------------------------------------------------Task 2.6
+// Pobierz pliki z zasobow
+// pobierz w js paragrafy, przycisk oraz div-a .square
+// stworz funkcje
+//#1: wyloguj w konsoli napis 'czesc'
+//#2: zmien kolor .square na czerwony
+//#3: zmien kolor .square na niebieski
+//#4: najpierw dodaj klase .show do 'Paragrafu 1' i usun klase .show z 'paragrafu 2', potem zrob na odwrot(toggle)
+// funkcje maja byc odpalana za pomoca:
+//#1 podwojnego klikniecia przycisku z burgerem
+//#2 wjechania mysza na div.square
+//#3 zjechania mysza z div.square
+//#4 klikniecia w przycisk z pizza
