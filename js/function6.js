f1d3644faf348839e92525eef36946e2542768fe
Igor Czyż <igorczyz13@gmail.com>
2023-02-13 16:12:54 +0100

    Downloading items

---
+++ js/function6.js
@@ -0,0 +1,16 @@
+// getElementById
+
+const test = document.getElementById('item')
+
+console.log(test);
+
+// getElementsByTagName
+
+const test2 = document.getElementsByTagName('ul')
+console.log(test2);
+
+
+// getElementsByClassName
+
+const test3 = document.getElementsByClassName('test')
+console.log(test3);
