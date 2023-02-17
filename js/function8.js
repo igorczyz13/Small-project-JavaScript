68602aa66085cc0f50cc86de1d37b6d3295328f2
Igor Czyż <igorczyz13@gmail.com>
2023-02-17 22:44:55 +0100

    Fix bug

--- js/function8.js
+++ js/function8.js
@@ -116,3 +116,17 @@ console.log({
     fruits,
     favFruits
 });
+
+// function addToArray(item, array) {
+//     const newArray = array.concat(item)
+
+//     return newArray
+// }
+
+// const fruits = ['apple', 'orange']
+// const favFruits = addToArray('banana', fruits)
+
+// console.log({
+//     fruits,
+//     favFruits
+// });
